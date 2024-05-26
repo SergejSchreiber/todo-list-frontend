6734f157062d47ecd6866068b5aaee14f84919d5
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-05-26 16:46:39 +0200

    drag drop

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -6,7 +6,7 @@ import { lastValueFrom } from 'rxjs';
 @Component({
   selector: 'app-all-todos',
   templateUrl: './all-todos.component.html',
-  styleUrl: './all-todos.component.scss'
+  styleUrls: ['./all-todos.component.scss']
 })
 export class AllTodosComponent {
   todos: any = [];
@@ -37,34 +37,65 @@ export class AllTodosComponent {
   }
 
   filterToDo(todos: any[]): any[] {
-    return todos.filter(p => p.status == "To-do");
+    return todos.filter(p => p.status === "To-do");
   }
 
   filterDoToday(todos: any[]): any[] {
-    return todos.filter(p => p.status == "Do today");
+    return todos.filter(p => p.status === "Do today");
   }
 
   filterInProcess(todos: any[]): any[] {
-    return todos.filter(p => p.status == "In Process");
+    return todos.filter(p => p.status === "In Process");
   }
 
   filterDone(todos: any[]): any[] {
-    return todos.filter(p => p.status == "Done");
+    return todos.filter(p => p.status === "Done");
   }
-  
 
-  addTodo() {
+  async addTodo() {
     const url = environment.baseUrl + "/todos/";
     const body = {
       "title": this.title,
       "description": this.description,
-      // "created_at": this.created_at,
-      "due_date": this.due_date,
+      "created_at": new Date().toISOString().split('T')[0], // format YYYY-MM-DD
+      "due_date": new Date(this.due_date).toISOString().split('T')[0], // format YYYY-MM-DD
       "priority": this.priority,
       "status": this.status,
-      "author":2
+      "author": 2
+    };
+    try {
+      const response = await lastValueFrom(this.http.post(url, body));
+      this.todos.push(response);
+    } catch (error) {
+      console.error(error);
+      this.error = 'Fehler beim Hinzufügen des Todos';
     }
-    return lastValueFrom(this.http.post(url, body));
   }
 
+  allowDrop(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  onDragStart(event: DragEvent, todo: any) {
+    event.dataTransfer?.setData("todo", JSON.stringify(todo));
+  }
+
+  async onDrop(event: DragEvent, newStatus: string) {
+    event.preventDefault();
+    const todo = JSON.parse(event.dataTransfer?.getData("todo") || '{}');
+    todo.status = newStatus;
+
+    const url = `${environment.baseUrl}/todos/${todo.id}/`;
+    const body = {
+      "status": newStatus
+    };
+    
+    try {
+      await lastValueFrom(this.http.patch(url, body));
+      this.todos = this.todos.map((t: any) => t.id === todo.id ? { ...t, status: newStatus } : t);
+    } catch (error) {
+      console.error(error);
+      this.error = 'Fehler beim Aktualisieren des Todo-Status';
+    }
+  }
 }
